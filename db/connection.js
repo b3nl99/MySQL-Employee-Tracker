58a8e08545df9411aebc8da9f0f935234a1a0efb
b3nl99 <blee0963@gmail.com>
2023-01-12 19:38:42 -0800

    main

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Th15_i5_My5QL.',
+    password: 'Your mysql password',
     database: 'employee_tracker'
 });
 
